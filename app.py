5dd81343248a60114bcfd786061397700f2da03a
chiranjeevi-star <chiranjeeviprasad34@gmail.com>
2023-04-08 15:19:27 +0530

    modified app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
-    return "Starting machine learning Project"
+    return "First diployment of chiranjeevi "
 
 if __name__=='__main__':
     app.run(debug=True)
